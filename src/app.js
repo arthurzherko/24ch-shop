ecade28dcf197a41f2477476519285531a4f4ef9
Arthur Zherko <arthurzherko@gmail.com>
2018-03-04 20:14:51 +0300

    Deploy in Heroku

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router'
-
-// new Vue({
-//   el: '#app',
-//   router,
-//   components: { App },
-//   render: h => h(App)
-// })
 import Vue from 'vue'
 import App from './App.vue'
 
